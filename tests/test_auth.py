e1f1f3aa3511c0fddf7cee195e1fa5aea92edc3d
Oscar Bautista <oscar.v.bautista@gmail.com>
2019-11-05 04:12:39 +0000

    Update authentication with 400 status codes

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,26 +1,28 @@
-import pytest
 from data.models import User
 
 
 def test_good_register(client, app):
-    """Register a new user.
-
-    Tests for validation that the username is not already taken.
-    """
+    """ Register a new user. Tests for validation that the username or email is not already taken. """
     res = client.post("/api/auth/register", data={"username": "a", "password": "a"})
-    assert res.status_code == 201  # Request okay! Account created.
+    assert res.status_code == 400  # Request is invalidly formatted and missing required data.
+    res = client.post("/api/auth/register", data={"username": "a", "password": "a", "email": "a@a.com"})
+    assert res.status_code == 200  # Request okay! Account created.
     with app.app_context():
         assert User.query.filter(User.username == 'a').first() is not None
-    res = client.post("/api/auth/register", data={"username": "a", "password": "a"})
+    res = client.post("/api/auth/register", data={"username": "a", "password": "a", "email": "b@a.com"})
     assert res.status_code == 403  # Forbidden to register existing username
+    res = client.post("/api/auth/register", data={"username": "b", "password": "a", "email": "a@a.com"})
+    assert res.status_code == 403  # Forbidden to register existing email
 
 
 def test_login(client):
-    """ Register a user and login with that user. Test with wrong password then right password"""
-    client.post("/api/auth/register", data={"username": "a", "password": "a"})
+    """ Register a user and login with that user. Test with wrong password then right password """
+    client.post("/api/auth/register", data={"username": "a", "password": "a", "email": "a@a.com"})
+    res = client.post("/api/auth/login", data={"username": "a"})
+    assert res.status_code == 400  # Request is invalidly formatted and missing required data
     res = client.post("/api/auth/login", data={"username": "a", "password": "b"})
-    assert res.status_code == 401
+    assert res.status_code == 401  # Password for existing user is incorrect
     res = client.post("/api/auth/login", data={"username": "a", "password": "a"})
-    assert res.status_code == 200
+    assert res.status_code == 200  # User is successfully authenticated.
 
 
