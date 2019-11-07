3aaa7267a84afc60b9ff1faed581a6926ae30412
Oscar Bautista <oscar.v.bautista@gmail.com>
2019-11-07 16:08:44 +0000

    Setup CI/CD

--- build/lib/data/Serializer.py
+++
@@ -1,8 +0,0 @@
-class Serializer(object):
-
-    def serialize(self):
-        return {c: getattr(self, c) for c in inspect(self).attrs.keys()}
-
-    @staticmethod
-    def serialize_list(data_list):
-        return [data.serialize() for data in data_list]
