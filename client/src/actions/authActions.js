522077cd9a495953c047ed2543e6702797e928bc
= <=>
2019-11-18 17:49:22 -0500

    not sure what i changed but somethign to do with auth ... forgot to commit awhile ago but it wasn't much

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,14 +1,19 @@
 import axios from 'axios';
 import {REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED, AUTH_ERROR, LOGIN_SUCCESS, LOGIN_FAIL, LOGOUT} from "./types";
 import setAuthToken from "../utils/setAuthToken";
+import setRefreshToken from "../utils/setRefreshToken";
 
 export const loadUser = () => async dispatch => {
     if (localStorage.access_token) {
         setAuthToken(localStorage.access_token);
     }
 
+    if (localStorage.refresh_token) {
+        setAuthToken(localStorage.refresh_token);
+    }
+
     try {
-        const res = await axios.get('http://localhost:5000/auth/login')
+        const res = await axios.get('http://localhost:5000/api/auth/login')
 
         dispatch({
             type: USER_LOADED,
@@ -32,7 +37,7 @@ export const register = ({ username, email, password }) => async dispatch => {
     const body = JSON.stringify({ username, email, password });
 
     try {
-        const res = await axios.post('http://localhost:5000/auth/register', body, config);
+        const res = await axios.post('http://localhost:5000/api/auth/register', body, config);
 
         dispatch({
             type: REGISTER_SUCCESS,
@@ -57,7 +62,7 @@ export const login = (username, password) => async dispatch => {
     const body = JSON.stringify({ username, password });
 
     try {
-        const res = await axios.post('http://localhost:5000/auth/login', body, config);
+        const res = await axios.post('http://localhost:5000/api/auth/login', body, config);
 
         dispatch({
             type: LOGIN_SUCCESS,
